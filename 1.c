39ff491f5b83bfc40b4530eb0e0fa47be61e77cc
SoaresGabriel <gabriel980500@gmail.com>
2024-11-07 16:14:08 -0300

    pequeno ajuste questão 1

--- 1.c
+++ 1.c
@@ -9,10 +9,6 @@ ele calcule a sequência de Fibonacci e retorne uma mensagem
 avisando se o número informado pertence ou não a sequência.
 */
 
-int qPf(int a) {
-
-}
-
 int main() {
     int n, a, b, R;
 
